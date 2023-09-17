ccf079d44b668c2f87d50655245f0c45ff5ac57c
Yunus Cem Özay <78884331+cemozay@users.noreply.github.com>
2023-09-17 17:33:17 +0300

    player ve spritelar eklendi.

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMov : MonoBehaviour
 {
     [SerializeField] private float flySpeed = 2f;
+    [SerializeField] private float rotationSpeed = 5f;
 
+    private Rigidbody2D rb;
+
+    void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * flySpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * rotationSpeed);
+    }
 }
